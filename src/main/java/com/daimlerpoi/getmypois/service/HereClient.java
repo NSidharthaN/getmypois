44a95df16c6d4ba222acfcc35fb41114edd074c0
SidN <nsidhartha@gmail.com>
2020-08-22 11:56:40 +0530

    incremental changes

--- src/main/java/com/daimlerpoi/getmypois/service/HereClient.java
+++ src/main/java/com/daimlerpoi/getmypois/service/HereClient.java
@@ -1,25 +1,66 @@
 package com.daimlerpoi.getmypois.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.PostConstruct;
-import java.util.Objects;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class HereClient {
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${herePlacesBaseUri}")
+    private String herePlacesBaseUri;
 
     @PostConstruct
     public void invokeHereApi() {
 
-        String url = "https://places.ls.hereapi.com/places/v1/browse?at=48.8465,2.3722&cat=restaurant%2Cparking-facility%2Cev-charging-station&Accept-Language=en-US%2Cen%3Bq%3D0.9&app_id=c0zGYugKrHb8Vppa8L31&app_code=FzyuK6KjQQGwZ88OW2umBg";
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-        Objects.nonNull(responseEntity);
-        System.out.println("Hey there \n \n"+responseEntity.getBody());
+
+        System.out.println("Hey there \n \n");
+
+
+        long start = System.nanoTime();
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        List<CompletableFuture<String>> futures =
+                Stream.of("restaurant", "parking-facility", "ev-charging-station")
+                        .map(t -> CompletableFuture.supplyAsync(() -> invokeHereApi(t), executor))
+                        .collect(Collectors.toList());
+
+        String result =
+                futures.stream()
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.joining(","));
+        long duration = (System.nanoTime() - start) / 1_000_000;
+        System.out.printf("Processed tasks in %d millis\n", duration);
+        System.out.println("Processed Response Result below ********* \n\n *****" + "[" + result + "]");
+        executor.shutdown();
+
+    }
+
+    public String invokeHereApi(String category) {
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(herePlacesBaseUri + "/v1/browse")
+                // Add query parameter
+                .queryParam("at", "48.8465,2.3722")
+                .queryParam("cat", category)
+                .queryParam("app_code", "FzyuK6KjQQGwZ88OW2umBg")
+                .queryParam("app_id", "c0zGYugKrHb8Vppa8L31")
+                .queryParam("size", "3");
+
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uriComponentsBuilder.build().toUri(), String.class);
+
+        return responseEntity.getBody();
     }
 
 
