22710b74f5ab4aa736726a5d7017621506632abe
SidN <nsidhartha@gmail.com>
2020-08-22 09:21:03 +0530

    initial commity

---
+++ src/main/java/com/daimlerpoi/getmypois/GetmypoisApplication.java
@@ -0,0 +1,13 @@
+package com.daimlerpoi.getmypois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmypoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmypoisApplication.class, args);
+	}
+
+}
