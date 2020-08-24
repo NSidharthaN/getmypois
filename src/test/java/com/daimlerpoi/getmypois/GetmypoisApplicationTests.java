4fbe401d2dd3b9a03bc6b83a94109f2204ddd4eb
SidN <nsidhartha@gmail.com>
2020-08-24 10:07:14 +0530

    incremental

--- src/test/java/com/daimlerpoi/getmypois/GetmypoisApplicationTests.java
+++ src/test/java/com/daimlerpoi/getmypois/GetmypoisApplicationTests.java
@@ -1,13 +1,31 @@
 package com.daimlerpoi.getmypois;
 
+import com.daimlerpoi.getmypois.entity.places.PlacesResponse;
+import com.daimlerpoi.getmypois.service.HereClient;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootTest
 class GetmypoisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    HereClient hereClient;
+
+    @Test
+    public void testHereClientResponseForRestaurantCategory() {
+        List<PlacesResponse> placesResponsesList = hereClient.invokeHereApi("London");
+        Assertions.assertTrue(Objects.nonNull(placesResponsesList) && placesResponsesList.size() > 0);
+        Assertions.assertTrue(placesResponsesList.get(0).getResults().getItems().size() > 1);
+        Assertions.assertTrue(placesResponsesList.get(0).getResults().getItems().stream().anyMatch(item -> "restaurant".equalsIgnoreCase(item.getCategory().getTitle())));
+    }
 
 }
